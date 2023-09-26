741ea2258e306b548a04e06a2edde22fad3302c2
laganin <laganin23@live.com>
2023-09-26 17:24:41 +0200

    add to-do list item add/remove test

---
+++ src/main/java/d25_09_2023/Zadatak2.java
@@ -0,0 +1,68 @@
+package d25_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+
+public class Zadatak2 {
+
+//Niz todo-a (niz stringova) koje treba da uneti. Niz je:
+//Visit Paris
+//Visit Prague
+//Visit London
+//Visit New York
+//Visit Belgrade
+//Maksimizirati prozor
+//Ucitati stranicu https://example.cypress.io/todo
+//Program petljom prolazi kroz niz todo-a i svaki unosi na stranicu
+//Nakon svakog unosa todo-a, unosi se enter. Koristan link
+//Nakon svih unosa proci petljom kroz svaki todo koji je na stranici i za svaki cekirati da je completed.
+//Cekanje od 5s
+//Zatvorite pretrazivac
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        ArrayList<String> todo = new ArrayList<>();
+        todo.add("Visit Paris");
+        todo.add("Visit Prague");
+        todo.add("Visit London");
+        todo.add("Visit New York");
+        todo.add("Visit Belgrade");
+
+        //*[@class='todo-list']//button
+
+        driver.navigate().to("https://example.cypress.io/todo");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".todo-list>li")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.className("destroy")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".todo-list>li")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.className("destroy")).click();
+        for (int i = 0; i < todo.size(); i++) {
+            driver.findElement(By.className("new-todo")).sendKeys(todo.get(i));
+            driver.findElement(By.className("new-todo")).sendKeys(Keys.ENTER);
+            Thread.sleep(2000);
+        }
+
+        for (int i = 0; i < todo.size(); i++) {
+            driver.findElement(By.cssSelector(".todo-list>li")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.className("destroy")).click();
+        }
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+}
